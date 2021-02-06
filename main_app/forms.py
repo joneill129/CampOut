41c60ff1baee86fa262731aefc51f31140ce78ac
jpaige343 <jenniferoneill129@gmail.com>
2021-02-06 17:25:44 -0600

    style add-trip form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import Trip
+from django import forms
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class TripForm(ModelForm):
     class Meta:
         model = Trip
         fields = ['startdate', 'enddate', 'reservation']
+        widgets = {
+            'startdate': DateInput(),
+            'enddate': DateInput()
+        }
